49bf97bf77ac1c05a8de33efd35ebc7a049804c2
Arshil Azeem <118335209+azeem2504@users.noreply.github.com>
2025-11-06 11:59:27 +0530

    Update model references in functions.ts

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -67,7 +67,7 @@ export const codeAgentFunction = inngest.createFunction(
         //     temperature: 0.1,
         //   },
         // model: "deepseek/deepseek-chat-v3-0324:free",
-        model: "mistralai/mistral‑7b‑instruct:free",
+        model: "mistralai/mistral-7b-instruct:free",
         apiKey: process.env.OPENROUTER_API_KEY,
         baseUrl: "https://openrouter.ai/api/v1",
       }),
@@ -212,7 +212,8 @@ export const codeAgentFunction = inngest.createFunction(
       system: RESPONSE_PROMPT,
       model: openai({
         // model: "deepseek/deepseek-chat-v3-0324:free",
-        model: "mistralai/mistral-7b-instruct:free" ,
+        model: "qwen/qwen-2.5-coder-32b-instruct:free",
+        // model: "mistralai/mistral-7b-instruct:free" ,
         apiKey: process.env.OPENROUTER_API_KEY,
         baseUrl: "https://openrouter.ai/api/v1",
       })
